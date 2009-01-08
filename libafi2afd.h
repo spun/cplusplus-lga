8d0053c457f1afdd55f862f5ae96d4845b279aab
Víctor Manuel González Mira <shadowfiltro@gmail.com>
2009-01-08 17:00:48 +0000

    Cambiado el vector de estados a tipo booleano

--- libafi2afd.h
+++ libafi2afd.h
@@ -1,6 +1,6 @@
 typedef char tVector[100][3];
 //En el primer campo almacena la renombnracion de los estados 
-typedef int tVectEstados[100][11];
+typedef bool tVectEstados[100][11];
 
 //El alfabeto de entrada del AFI tendrá como mucho 5 simbolos
 typedef char tVectCaracteres[5];
@@ -10,7 +10,7 @@ bool ProcesarLetra(char letra);
 void AlmacenaLetra(char l, bool e, int &lin, int &fil, tVector vOrig);
 void BuscarInicios(tVector Orig, tVectEstados Estados, int lineas);
 void Buscar(tVector Orig, tVectEstados Estados, int lineas,tVectCaracteres VectCaract, int numCaract);
-bool BuscarRepetidos (tVectEstados Estados);
+bool BuscarRepetidos (tVectEstados Estados, int a, int b);
 
 void CaracteresAFI(tVector Orig, int lineas, tVectCaracteres Caracteres, int &numCaracteres);
 
